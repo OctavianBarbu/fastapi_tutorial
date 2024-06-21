389bc76bfe63a31be6a7e9fc6b6b71214bafc2dc
Barbu Octavian <barbu.octavian01@gmail.com>
2024-06-22 00:18:24 +0300

    added get users endpoint

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,12 +1,21 @@
-from .. import schemas, models, utils
+from .. import schemas, models, utils, oath2
 from fastapi import FastAPI, HTTPException, Response, status, Depends, APIRouter
 from sqlalchemy.orm import Session
 from ..database import get_db
+from typing import Optional, List
 
 router = APIRouter(
     prefix="/users",
     tags=["Users"]
 )
+
+
+@router.get("/", response_model= List[schemas.UserResponse])
+def get_users(db: Session = Depends(get_db), current_user: int = 
+                 Depends(oath2.get_current_user)):
+    users = db.query(models.User).all()
+    return users
+
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
@@ -25,7 +34,8 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     
 
 @router.get("/{id}", response_model=schemas.UserResponse)
-def get_user(id: int, db: Session = Depends(get_db)):
+def get_user(id: int, db: Session = Depends(get_db), current_user: int = 
+                 Depends(oath2.get_current_user)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
